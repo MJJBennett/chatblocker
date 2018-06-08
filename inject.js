6cbc94540d8fa96d54489c7a219a3d1466da37e7
Michael Bennett <michaelbennett.dev@gmail.com>
2018-06-07 19:13:40 -0700

    Trying to get it working again except with a json blocklist instead of hardcoded.

--- inject.js
+++ inject.js
@@ -5,8 +5,8 @@
  * This script was created with some code & inspiration from the following tutorial:
  * http://www.nhatqbui.com/programming/2016/11/22/howtobuildatwitchchatchromeext.html
  * 
- * Version 0.1.0:
- *   - Automatically removes all Twitch chat messages with text content
+ * Version 0.1.1:
+ *   - Removes messages if the is_blocked(text, sender) returns true for that message. 
  * 
  * Future development should allow for the creation of regexes in order to properly parse
  * and remove messages based on word, text, and username blacklists (and, potentially, 
@@ -17,13 +17,25 @@
 var html_body = $("body")[0];
 
 //This may not be necessary:
-//debugger;
+debugger;
+console.log("hi");
 
 //Some persistent variables
 var hidden_messages = 0;
 
-var is_blocked = function(text, sender) {
 
+const url = chrome.runtime.getUrl('config.json');
+const response = await fetch(url);
+const config = await response.json();
+
+var is_blocked = function(text, sender) {
+    for (var i = 0; i < config.block_if_contains.length; i++) {
+        if (text.includes(config.block_if_contains[i])) return true;
+    }
+    for (var i = 0; i < config.block_if_user.length; i++) {
+        if (sender == config.block_if_user[i]) return true;
+    }
+    return false;
 };
 
 var chatLoadedObserver = new MutationObserver(function (mutations, observer) {
@@ -42,26 +54,19 @@ var chatLoadedObserver = new MutationObserver(function (mutations, observer) {
 
 var chatObserver = new MutationObserver(function (mutations) {
     mutations.forEach(function(mutation) {
-        console.log("Chat updated!");
+        //console.log("Chat updated!");
         mutation.addedNodes.forEach(function (addedNode) {
             var chatMessage = $(addedNode);
             if (!chatMessage.is(".chat-line__message")) return;
-            //chatMessage = chatMessage[0];
-            console.log(chatMessage);
-            data = chatMessage.children("span[data-a-target='chat-message-text']").each(function (child) {
-                //child_message = child[0];
-                //console.log("Modifying text.");
-                //$(child).text('HELLO!!!');
-                //child.parentElement.removeChild(child);
-                return [child];
-            });
-            if (text.length < 1) {
-                console.log("Ignored one thing.");
-                return;
+            //console.log(chatMessage);
+            text = chatMessage.children("span[data-a-target='chat-message-text']").text();
+            sender = chatMessage.children(".chat-line__username").find(".chat-author__display-name").attr("data-a-user");
+            //console.log(sender);
+            //console.log(text);
+            if (is_blocked(text, sender)) {
+                if (++hidden_messages %2 == 0) console.log("Have now removed " + hidden_messages + " messages!"); 
+                chatMessage.remove();
             }
-            console.log($(text).text());
-            text.parent().remove();
-            //parseMsgHTML(messageElement);
         });
     });
 });
